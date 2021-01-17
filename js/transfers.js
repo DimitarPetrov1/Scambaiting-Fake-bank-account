ac4949c5ffab112634e43689c07b03e6729fbcf1
Dimitar Petrov <60468978+DimitarPetrov1@users.noreply.github.com>
2021-01-17 20:12:31 +0200

    Added a Modal Plus open and close fundtion

--- js/transfers.js
+++ js/transfers.js
@@ -1,10 +1,41 @@
-// Transfer buttons + event listener + function to retrieve the ID of the HTML button element so the relevant account can be selected
-let transferButtons = document.querySelectorAll(".transferBtn");
-transferButtons.forEach((button) => {
-  button.addEventListener("click", getBtnIds);
-});
-function getBtnIds(e) {
-  let getButtonId = e.target.getAttribute("id");
-  console.log(getButtonId);
+// Getting account buttons + the amount of each account + adding event listener for the account buttons
+let transferButtons = document.querySelectorAll("._account .transferBtn");
+// Getting the Modal for transfers + open and close function
+let rootModal = document.getElementById("rootModal");
+let modalTemplate = `
+<form class="modal modal-open">
+  <p>namee</p>
+    <div class="modal-separator">
+      <input id="transferAmount" type="number" />
+      <button id="submitBtn" type="submit">Submit</button>
+    </div>
+  <img class="closeBtn" src="img/svg/x.svg" alt="X" />
+</form>
+`;
+rootModal.innerHTML = modalTemplate;
+let closeModalBtn = document.querySelector(".closeBtn");
+closeModalBtn.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  let target = document.querySelector(".modal");
+  if (target.classList.contains("modal-open")) {
+    target.classList.remove("modal-open");
+  } else {
+    target.classList.add("modal-open");
+  }
 }
 //
+
+transferButtons.forEach((button) => {
+  button.addEventListener("click", getTransferDetails);
+});
+function getTransferDetails(e) {
+  e.preventDefault();
+  let att = this.parentElement.parentElement;
+  toggleModal();
+  if (att.hasAttribute("value")) {
+    console.log(att.getAttribute("value"));
+  } else {
+    return null;
+  }
+}
